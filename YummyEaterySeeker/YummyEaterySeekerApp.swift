e7bb3dee378d156d916b03b27e43ea8128205862
khatch2_name <khatchshah2@gmail.com>
2023-10-27 13:32:46 +0200

    Initial Commit

---
+++ YummyEaterySeeker/YummyEaterySeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummyEaterySeekerApp.swift
+//  YummyEaterySeeker
+//
+//  Created by Khatch Shah on 2023-10-27.
+//
+
+import SwiftUI
+
+@main
+struct YummyEaterySeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
