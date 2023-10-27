11a956c3f1b5c2ccdc603dd8391c93f0929c1737
khatch2_name <khatchshah2@gmail.com>
2023-10-27 15:46:44 +0200

    Created Restaurant.swift

--- YummyEaterySeeker/ContentView.swift
+++ YummyEaterySeeker/ContentView.swift
@@ -6,14 +6,33 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 struct ContentView: View {
+    
+    var dbFirestore = Firestore.firestore()
+    
+    @State var restaurantName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            
+            Text("Add a new restaurant").padding()
+            
+            TextField("Enter the name of a restaurant?", text: $restaurantName).textFieldStyle(.roundedBorder).padding()
+            
+            Button(action: {
+                if (!restaurantName.isEmpty) {
+                    dbFirestore.collection("Created_Restaurants").document("New_Restaurant").setData(["name": restaurantName])
+                }
+//                dbFirestore.collection("test1").addDocument(data: ["name": "Tony Tomas"])
+                dbFirestore.collection("test2").document("david").setData(["Ulvan": "Student"])
+            }, label: {
+                Text("Create an entry in Firestore")
+            })
         }
         .padding()
     }
---
+++ YummyEaterySeeker/Restaurant.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant.swift
+//  YummyEaterySeeker
+//
+//  Created by Khatch Shah on 2023-10-27.
+//
+
+import SwiftUI
+import FirebaseFirestoreSwift
+
+struct Restaurant: Codable {
+    
+    @DocumentID var id: String?
+    
+    }
+
+
