8b15f5b80cd3cc8f60dc84c6c6431c3e65dd3aa2
khatch2_name <khatchshah2@gmail.com>
2023-10-28 21:00:27 +0200

    Created Resturant ViewModel, i.e.: YummyVM.swift

---
+++ YummyEaterySeeker/ViewModels/YummyVM.swift
@@ -0,0 +1,18 @@
+//
+//  YummyVM.swift
+//  YummyEaterySeeker
+//
+//  Created by Khatch Shah on 2023-10-28.
+//
+
+import SwiftUI
+
+struct YummyVM: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YummyVM()
+}
