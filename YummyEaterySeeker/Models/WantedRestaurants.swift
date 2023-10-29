8b0eb312d96af9198af0f4097ef7ab5fb0c907f5
khatch2_name <khatchshah2@gmail.com>
2023-10-29 19:28:31 +0100

    Created WantedRestaurant.swift

---
+++ YummyEaterySeeker/Models/WantedRestaurants.swift
@@ -0,0 +1,25 @@
+//
+//  WantedRestaurants.swift
+//  YummyEaterySeeker
+//
+//  Created by Khatch Shah on 2023-10-29.
+//
+
+import Foundation
+import MapKit
+
+import SwiftUI
+
+
+
+struct WantedRestaurants: View {
+    
+    var body: some View {
+        Text("jesus")
+    }
+    
+}
+
+#Preview {
+    WantedRestaurants()
+}
