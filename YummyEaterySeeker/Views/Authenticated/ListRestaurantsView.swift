92032323a382b9679c75784f79d00ebca51e0206
khatch2_name <khatchshah2@gmail.com>
2023-11-02 20:47:23 +0100

    Done: researching on copyDocument()

--- YummyEaterySeeker/Views/Authenticated/ListRestaurantsView.swift
+++ YummyEaterySeeker/Views/Authenticated/ListRestaurantsView.swift
@@ -8,6 +8,29 @@
 
 import Foundation
 import SwiftUI
+import Firebase
+//import FirebaseSwiftUI
+
+func copyDocument(from sourceCollection: String, sourceDocument: String, to targetCollection: String, targetDocument: String) {
+    let sourceRef = Firestore.firestore().collection(sourceCollection).document(sourceDocument)
+    let targetRef = Firestore.firestore().collection(targetCollection).document(targetDocument)
+
+    sourceRef.getDocument { (document, error) in
+        if let error = error {
+            print("Error fetching source document: \(error.localizedDescription)")
+        } else if let document = document, document.exists {
+            targetRef.setData(document.data() ?? [:]) { error in
+                if let error = error {
+                    print("Error copying document: \(error.localizedDescription)")
+                } else {
+                    print("Document copied successfully.")
+                }
+            }
+        } else {
+            print("Source document does not exist.")
+        }
+    }
+}
 
 
 struct ListRestaurantsView: View {
@@ -29,7 +52,21 @@ struct ListRestaurantsView: View {
                         if let userData = db.currentUserData {
                             
                             if userData.restaurants.count < 1 {
+                                
                                 Text("No restaurants yet!")
+                                
+                                // Button kopiera
+                                Button(action: {
+                                    
+                                    copyDocument(from: "restaurants_in_near_liljeholmen", sourceDocument: "55bsCPMGDCcBUF7r9hQU", to: "user_data", targetDocument: "IGvMj2FX7gNMFsLRCu5zhbOBxyW2")
+                                    
+                                    ListRestaurantsView(db: DbConnection())
+                                    
+                                }, label: {
+                                    Text("Kopiera från Liljeholmen").bold().padding().background(.yellow).cornerRadius(9)
+                                })
+                                
+                                
                             } else {
                                 List() {
                                     ForEach(userData.restaurants) { restaurant in
@@ -44,31 +81,48 @@ struct ListRestaurantsView: View {
                                     }
                                 }
                                 
-                                NavigationLink(destination: RegisterView(db: db), label: {
-                                    VStack {
-                                        HStack {
-                                            Text("Goto Registration")
-                                    } }
-                                })
-                                
-                                Button(action: {
-                                    do {
-                                        try db.auth.signOut()
-                                    } catch let signOutError as NSError {
-                                        print("Error signing out: %@", signOutError)
+                                HStack {
+                                    
+                                    NavigationLink(destination: RegisterView(db: db), label: {
+                                        VStack {
+                                            HStack {
+                                                Text("Goto Registration").bold().padding().background(.yellow).cornerRadius(9)
+                                        } }
+                                    })
+                                    
+                                    Button(action: {
+                                        do {
+                                            try db.auth.signOut()
+                                        } catch let signOutError as NSError {
+                                            print("Error signing out: %@", signOutError)
 
-                                    }
-                                }, label: {
-                                    Text("Log out me")
-                                })
+                                        }
+                                    }, label: {
+                                        Text("Log out me").bold().padding().background(.yellow).cornerRadius(9)
+                                    })
+                                    
+                                }
                                 
-                                NavigationLink(destination: LoginView(db: db), label: {
-                                    VStack {
-                                        HStack {
-                                            Text("Goto Login")
-                                    } }
-                                })
+                                HStack {
+                                    
+                                    NavigationLink(destination: LoginView(db: db), label: {
+                                        VStack {
+                                            HStack {
+                                                Text("Goto Login").bold().padding().background(.yellow).cornerRadius(9)
+                                        } }
+                                    })
+                                    
+                                    Button(action: {
+                                        
+                                        copyDocument(from: "restaurants_in_near_liljeholmen", sourceDocument: "55bsCPMGDCcBUF7r9hQU", to: "user_data", targetDocument: "lhFJXYt0bjVr8v8vRUT13TesWHZ2")
+                                        
+                                    }, label: {
+                                        Text("Copy from Liljeholmen").bold().padding().background(.yellow).cornerRadius(9)
+                                    })
+
+                                }
                                 
+
                             }
                             
                         } else {
